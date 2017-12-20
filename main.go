4edf898c9bd20b20197231877ddae9aac5e2079f
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-12-20 15:49:32 -0500

    Initializing

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 var db = "."
@@ -22,7 +24,29 @@ func main() {
 }
 
 func exchangeCode(code string) bool {
-	fmt.Println(code)
+	req, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", strings.NewReader("grant_type=authorization_code&code="+code+"&redirect_uri=http://localhost:3456/catch"))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "Basic NzE1YzE1ZmM3NTAzNDAxZmIxMzZkNmE3OTA3OWI1MGM6ZTkxZWZkZDAzNDVkNDlkNTllOGE2ZDc1YjUzZTE2YTE=")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	bodyMap := make(map[string]interface{})
+
+	err = json.Unmarshal(body, &bodyMap)
+	if err != nil {
+		panic(err)
+	}
+
+	write("accessBearer", "Bearer "+bodyMap["access_token"].(string))
+	write("refreshBody", "grant_type=refresh_token&refresh_token="+bodyMap["refresh_token"].(string))
+
 	return true
 }
 
@@ -44,4 +68,14 @@ func serve(done chan bool) {
 		done <- exchangeCode(code)
 	})
 	http.ListenAndServe(":3456", nil)
+}
+
+func write(name, content string) {
+	target := db + name
+	f, err := os.Create(target)
+	if err != nil {
+		// return err
+	}
+	defer f.Close()
+	f.WriteString(content)
 }
